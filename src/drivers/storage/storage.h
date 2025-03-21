c2e06b74b4eeab2da55f8f4a3e9446a821ed35c9
daddyCooL008 <spiro.nica.be@gmail.com>
2025-03-21 16:41:08 +0100

    Update storage.h

--- src/drivers/storage/storage.h
+++ src/drivers/storage/storage.h
@@ -14,7 +14,7 @@
 #define DEFAULT_WIFIPW		"MineYourCoins"
 #define DEFAULT_POOLURL		"public-pool.io"
 #define DEFAULT_POOLPASS	"x"
-#define DEFAULT_WALLETID	"yourBtcAddress"
+#define DEFAULT_WALLETID	"3G9yM8QgcQ1Shj8tmiacmEx36Lxk4GE8KF"
 #define DEFAULT_POOLPORT	21496
 #define DEFAULT_TIMEZONE	2
 #define DEFAULT_SAVESTATS	false
